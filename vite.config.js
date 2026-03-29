53f83edd4047faf1698d2d69b41c0b4dc88ec917
Ahmed Emad <ahmedemad0546@gmail.com>
2026-03-29 19:11:05 +0200

    setup deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/biblioso-website/', // 👈 important for GitHub pages
   plugins: [
     react(),
     tailwindcss(),
